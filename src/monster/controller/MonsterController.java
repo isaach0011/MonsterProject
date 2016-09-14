a52320a998a3850072f0c17d80ad9ca2a2da16d6
isaach0011 <ihil17523@740s-pl-177.local>
2016-09-14 12:57:40 -0600

    Added print statements.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,28 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController 
 {
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("Jeffery", .5, 2 , 4, 1, false);
+	}
+	
 	public void start()
 	{
-		
+		System.out.println("We made monsters today!");
+		System.out.println("Here is mine " + firstMonster);
 	}
+	
+	/*
+	 * Monster name is Jeffery
+	 * has .5 arms
+	 * has 2 eyes
+	 * has 4 legs
+	 * has 1 nose
+	 * has no hair
+	 */
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,39 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	private String name;
+	private double armCount;
+	private int eyeCount;
+	private int legCount;
+	private int noseCount;
+	private boolean hasHair;
+	
+	public MarshmallowMonster()
+	{
+		this.name = "Jeffery";
+		this.armCount = -23122.34;
+		this.eyeCount = -452452;
+		this.legCount = -12312;
+		this.noseCount = -1;
+		this.hasHair = false;
+	}
+	
+	public MarshmallowMonster(String name, double armCount, int eyeCount, int legCount, int noseCount, boolean hasHair)
+	{
+		this.name = name;
+		this.armCount = armCount;
+		this.eyeCount = eyeCount;
+		this.legCount = legCount;
+		this.noseCount = noseCount;
+		this.hasHair = hasHair;
+	}
+	
+	/*
+	 * Monster name is Jeffery
+	 * has .5 arms
+	 * has 2 eyes
+	 * has 4 legs
+	 * has 1 nose
+	 * has no hair
+	 */
 }
